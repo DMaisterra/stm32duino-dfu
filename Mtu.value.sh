10448c18571d0beb04cbef83c64aa7fc6621b21f
DMaisterra <david.maisterra@hotmail.com>
2022-03-07 15:17:00 -0300

    Ultimate version

--- Mtu.value.sh
+++ Mtu.value.sh
@@ -1,4 +1,15 @@
-awk 'NR==29' ~/.arduino15/packages/stm32duino/tools/stm32tools/2021.5.31/linux/maple_upload
-echo This last value  in effects of a reseting dfu board
-echo ...
-echo If the board dont enters dfu mode try other.
+echo "Port ttyACMx <- 0,1,2"
+echo "enter value"
+read nb
+PORT="/dev/ttyACM"$nb
+for ((i=1;i<=50;i++)); do
+	let "DELAY = 50 * i + 1500"
+	echo "DELAY = $DELAY"
+	~/.arduino15/packages/stm32duino/tools/stm32tools/2021.5.31/linux/upload-reset $PORT $DELAY
+	~/.arduino15/packages/stm32duino/tools/stm32tools/2021.5.31/linux/dfu-util/dfu-util -l
+	
+	COUNTER=0
+	while [ ! -r $PORT ] && ((COUNTER++ < 40)); do
+		sleep 0.1
+	done
+done
